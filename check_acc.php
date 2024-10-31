26313fd20c9802a5349ee23dff6d1813f4740591
Manda <mandashen@gmail.com>
2024-10-31 22:27:53 +0800

    修改check_acc錯誤

--- check_acc.php
+++ check_acc.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+
 if(!isset($_POST['acc'])){
     header("location:login.php");
     exit();
@@ -10,8 +10,8 @@ $password=$_POST['password'];
 
 if($acc=='admin' && $password=='1234'){
     echo "登入成功";
-    setcookie("login","$acc",time()+60);
-    echo $_COOKIE['login'];
+    setcookie("login",$acc,time()+60);
+    // echo $_COOKIE['login'];
     echo "<br><a href='login.php?login=$acc'>回首頁</a>";
 
 }else{
