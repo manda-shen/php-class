b3496057b9a1dc60630534f4e46b13fde469d82e
Manda <mandashen@gmail.com>
2024-10-31 23:14:48 +0800

    增加CSS樣式

--- include/common/navbar.php
+++ include/common/navbar.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+
+    </style>
+</head>
+<body>
+
+
 <?php
 
 $header=[
@@ -9,7 +22,10 @@ $header=[
 ];
 
 ?>
-<h1><?=$header[$page];?></h1>
+
+<header>
+<h3><?=$header[$page];?></h3>
+</header>
 <?php
 foreach($header as $key => $value){
     echo "<a class='".($page==$key?'now-page':'')."' href='{$key}.php'>{$value}</a>"."&nbsp;&nbsp;&nbsp;";
@@ -17,8 +33,13 @@ foreach($header as $key => $value){
 
 ?>
 
+
 <!-- <a class="<?=($page=='index')?'now-page':'';?>" href="index.php">首頁</a>
     <a class="<?=($page=='about')?'now-page':'';?>" href="about.php">關於我們</a>
     <a class="<?=($page=='product')?'now-page':'';?>" href="product.php">產品介紹</a>
     <a class="<?=($page=='contact')?'now-page':'';?>" href="contact.php">聯絡我們</a>
     <a class="<?=($page=='login')?'now-page':'';?>" href="login.php">登入</a> -->
+
+
+</body>
+</html>
